175021b5b63cafc568b824efb37186a13512331e
Xavier Valarino <xvalarino@gmail.com>
2022-05-30 17:46:10 -0500

    Remove comments from quickstart code

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,8 @@
 import TextCollection from "./text-collection";
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
 console.clear();
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
 
 const collection = new TextCollection();
 
